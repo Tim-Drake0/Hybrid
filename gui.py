b98aa06e465a4e28122f82323a5a15b4497d8b44
Tim Drake <tim@timdrake.org>
2025-09-06 20:31:17 -0700

    updated fonts, added voltage plot

--- gui.py
+++ gui.py
@@ -5,9 +5,10 @@ import csv
     
 df = pd.read_csv("data12.csv")
 dpg.create_context()
-#dpg.toggle_viewport_fullscreen()
-    
-    
+
+with dpg.font_registry():     
+    default_font = dpg.add_font("Assets/RobotoMono-Regular.ttf", 25)
+
 def toggle_plot_visibility(sender, app_data):
     """Callback function to toggle the plot's visibility."""
     # Toggle the plot's visibility using configure_item
@@ -15,16 +16,23 @@ def toggle_plot_visibility(sender, app_data):
         dpg.configure_item("pressure_plot", show= True)
         dpg.configure_item("discrete_plot", show= False)
         dpg.configure_item("load_cell_plot", show= False)
+        dpg.configure_item("voltage_plot", show= False)
     if app_data == 538:
         dpg.configure_item("pressure_plot", show= False)
         dpg.configure_item("discrete_plot", show= True)
         dpg.configure_item("load_cell_plot", show= False)
+        dpg.configure_item("voltage_plot", show= False)
     if app_data == 539:
         dpg.configure_item("pressure_plot", show= False)
         dpg.configure_item("discrete_plot", show= False)
         dpg.configure_item("load_cell_plot", show= True)
-        
-    
+        dpg.configure_item("voltage_plot", show= False)
+    if app_data == 540:
+        dpg.configure_item("pressure_plot", show= False)
+        dpg.configure_item("discrete_plot", show= False)
+        dpg.configure_item("load_cell_plot", show= False)
+        dpg.configure_item("voltage_plot", show= True)
+   
     
 df["Time[ms]"] = round(df['Time[ms]'].multiply(0.001),1)
 time_list = df["Time[ms]"].to_list()
@@ -46,6 +54,10 @@ py2 = (df['PY2'] + 0).to_list()
 
 load_cell = df['LC[lbf]'].to_list()
 
+voltage_batt = df['BATT[V]'].to_list()
+voltage_5v = df['5V[V]'].to_list()
+voltage_radio = df['RADIO[V]'].to_list()
+
 op_time = time_list[100:1425]
    
  
@@ -66,15 +78,13 @@ dpg.create_viewport(title='Custom Title',
 with dpg.window(label="Plots",
                 no_title_bar = True,
                 width=1080, 
-                height=700,
+                height=720,
                 pos = [200,0]):
 
-    window_dim = [1050, 640]
-    plot_dim = [1050, 620]
-    #with dpg.menu_bar():
-        # Pressure plot
-        #with dpg.menu(label="Pressure", show = True):
-        #    with dpg.child_window(height=window_dim[1], width=window_dim[0], show = True, tag="my_plot"):
+    window_dim = [1050, 720]
+    plot_dim = [1050, 690]
+
+    # Pressure plot
     with dpg.plot(label="Pressure Transducers", height=plot_dim[1], width=plot_dim[0], tag="pressure_plot"):
         dpg.add_plot_legend()
         # REQUIRED: create x and y axes
@@ -89,10 +99,8 @@ with dpg.window(label="Plots",
         #dpg.add_line_series(op_time, pt5, label="PT5", parent="y_axis_p")
         #dpg.add_line_series(op_time, pt6, label="PT6", parent="y_axis_p")
                     
-        # Discretes Plot
-        #with dpg.menu(label="Discretes"):
-        #    with dpg.child_window(height=window_dim[1], width=window_dim[0]):
-    with dpg.plot(label="Discrete Logic", height=plot_dim[1], width=plot_dim[0], tag = "discrete_plot"):
+    # Discretes Plot
+    with dpg.plot(label="Discrete Logic", height=plot_dim[1], width=plot_dim[0], tag = "discrete_plot", show = False):
         dpg.add_plot_legend()
         # REQUIRED: create x and y axes
         dpg.add_plot_axis(dpg.mvXAxis, label="Time [sec]")
@@ -109,50 +117,51 @@ with dpg.window(label="Plots",
         dpg.add_line_series(op_time, py1, label="PY1", parent="y_axis")
         dpg.add_line_series(op_time, py2, label="PY2", parent="y_axis")
                     
-        # Load Cell Plot
-        #with dpg.menu(label="Load Cell"):
-        #    with dpg.child_window(height=window_dim[1], width=window_dim[0]):
-    with dpg.plot(label="Load Cell Thrust", height=plot_dim[1], width=plot_dim[0], tag = "load_cell_plot"):
+     # Load Cell Plot
+    with dpg.plot(label="Load Cell Thrust", height=plot_dim[1], width=plot_dim[0], tag = "load_cell_plot", show = False):
         dpg.add_plot_legend()
         # REQUIRED: create x and y axes
         dpg.add_plot_axis(dpg.mvXAxis, label="Time [sec]")
         dpg.add_plot_axis(dpg.mvYAxis, label="Thrust [Lbf]", tag="y_axis_t")
-        #dpg.set_axis_limits(dpg.mvYAxis, 0, 16)
 
         # lines
         dpg.add_line_series(op_time, load_cell, label="Load Cell", parent="y_axis_t")
+        
+    # Voltage plot
+    with dpg.plot(label="Voltages", height=plot_dim[1], width=plot_dim[0], tag="voltage_plot"):
+        dpg.add_plot_legend()
+        # REQUIRED: create x and y axes
+        dpg.add_plot_axis(dpg.mvXAxis, label="Time [sec]")
+        dpg.add_plot_axis(dpg.mvYAxis, label="Voltage [V]", tag="y_axis_v")
 
+        # lines
+        dpg.add_line_series(op_time, voltage_batt, label="BATT", parent="y_axis_v")
+        dpg.add_line_series(op_time, voltage_5v, label="5V", parent="y_axis_v")
+        dpg.add_line_series(op_time, voltage_radio, label="RADIO", parent="y_axis_v") 
+
+    dpg.bind_font(default_font)
+    
 with dpg.window(label="Stats",
                 no_title_bar = True,
                 width=200, 
-                height=700,
+                height=720,
                 pos = [0,0]):
     with dpg.table(header_row=False):
-
-        # use add_table_column to add columns to the table,
-        # table columns use slot 0
         dpg.add_table_column()
-        dpg.add_table_column()
-
-        # add_table_next_column will jump to the next row
-        # once it reaches the end of the columns
-        # table next column use slot 1
         with dpg.table_row():
-
             dpg.add_text("Burn Time")
-            dpg.add_text(str(5)+" sec")
+        with dpg.table_row():
+            dpg.add_text("   "+str(5)+" sec")
             
         with dpg.table_row():
+            dpg.add_text(f"Max Thrust: {max_thrust} lbf")
             
-            dpg.add_text("Max Thrust")
-            dpg.add_text(str(max_thrust)+" lbf")
-            
+         
+    dpg.bind_font(default_font) 
 # Register the key press handler
 with dpg.handler_registry():
-    # Bind the 'P' key (mvKey_P) to the callback function
     dpg.add_key_press_handler(callback=toggle_plot_visibility)
-            
-
+    dpg.add_key_press_handler(dpg.mvKey_F, callback=dpg.toggle_viewport_fullscreen())
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
