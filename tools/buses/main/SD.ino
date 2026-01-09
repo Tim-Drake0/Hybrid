adf2018982f8878be61759b3ad015207e24a7361
Tim Drake <tim@timdrake.org>
2026-01-08 23:34:56 -0800

    fixed filename generation. has to be at most 10 chars long

--- tools/buses/main/SD.ino
+++ tools/buses/main/SD.ino
@@ -15,33 +15,40 @@ bool sd_present = false;
 
 File myFile;
 
-int SD_writeFreq = 20; // Hz
+int SD_writeFreq = 30; // Hz
 int SD_flushFreq = 5; // seconds
 uint32_t SD_timeLastWrite = 0UL;
 uint32_t SD_timeLastFlush = 0UL;
-char filename[32];
+char filename[10];
 
 void beginSD(){
     if (SD.begin(CS_SD_pin)){
-        bitSet(thisFrame.sensorsBIT, 4); 
         sd_present = true; 
     }
 
     if(sd_present){
         
-        strcpy(filename, "datalog.bin"); // base filename
-        if(SD.exists(filename)){
+        strcpy(filename, "data.bin"); // base filename
+        if (SD.exists(filename)) {
+            bool found = false;
             for (int i = 1; i <= MAX_FILE_INDEX; i++) {
-                sprintf(filename, "datalog%03d.bin", i);
+                sprintf(filename, "data%03d.bin", i);
                 if (!SD.exists(filename)) {
+                    found = true;
                     break; // found a free filename
                 }
             }
+            if (!found) {
+                return;
+            }
         }
 
-        myFile = SD.open(filename, FILE_WRITE);
+        // Set bit only when SD present and found valid file
+        bitSet(thisFrame.sensorsBIT, 4); 
 
         // might want to write some info at the top of the file, or maybe a separate config output file?
+
+        myFile = SD.open(filename, FILE_WRITE | O_CREAT | O_EXCL);
     }
 }
 
